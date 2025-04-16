4cab0dc848285264299cca370719c7b6cbcd3fbe
Arunprabhakaran04 <arunprabhakaran1729@gmail.com>
2025-04-16 18:58:19 +0530

    individual_stock_analysis

---
+++ individual_anlaysis.R
@@ -0,0 +1,7 @@
+#Datasets of various companies downloaded from Kaggle.
+#link to kaggle - https://www.kaggle.com/datasets/camnugent/sandp500
+
+df_apple <- read.csv("D:\\R\\credit-based loan approval\\individual_stocks_5yr\\individual_stocks_5yr\\AAPL_data.csv")
+df_nvidia <- read.csv("D:\\R\\credit-based loan approval\\individual_stocks_5yr\\individual_stocks_5yr\\NVDA_data.csv")
+print(head(df_apple))
+print(head(df_nvidia))
